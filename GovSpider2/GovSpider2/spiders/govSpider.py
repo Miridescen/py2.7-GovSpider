71b8e07ea198f5fb9c9e89f173105d068fabf703
mu.song@trs.com.cn <mu.song@trs.com.cn>
2019-03-26 18:21:48 +0800

    修改python2与python3的差别代码

--- GovSpider2/GovSpider2/spiders/govSpider.py
+++ GovSpider2/GovSpider2/spiders/govSpider.py
@@ -12,25 +12,25 @@ class govSpider(CrawlSpider):
     custom_settings = {'LOG_LEVEL': 'INFO', }
 
     start_urls = ["http://www.gov.cn/banshi/wjrs/crj.htm",
-                  "http://www.gov.cn/banshi/wjrs/hysy.htm",
-                  "http://www.gov.cn/banshi/wjrs/lssf.htm",
-                  "http://www.gov.cn/banshi/wjrs/lygg.htm",
-                  "http://www.gov.cn/banshi/wjrs/swtz.htm",
-                  "http://www.gov.cn/banshi/wjrs/whjy.htm",
-                  "http://www.gov.cn/banshi/wjrs/ymdj.htm",
-                  "http://www.gov.cn/banshi/wjrs/zhjy.htm",
-                  "http://www.gov.cn/banshi/wjrs/zscq.htm",
-                  "http://www.gov.cn/banshi/zjpq.htm",
-                  "http://www.gov.cn/banshi/wjrs/crj/wtjd.htm",
-                  "http://www.gov.cn/banshi/wjrs/hysy/wtjd.htm",
-                  "http://www.gov.cn/banshi/wjrs/lssf/wtjd.htm",
-                  "http://www.gov.cn/banshi/wjrs/lygg/wtjd.htm",
-                  "http://www.gov.cn/banshi/wjrs/swtz/wtjd.htm",
-                  "http://www.gov.cn/banshi/wjrs/whjy/wtjd.htm",
-                  "http://www.gov.cn/banshi/wjrs/ymdj/wtjd.htm",
-                  "http://www.gov.cn/banshi/wjrs/zhjy/wtjd.htm",
-                  "http://www.gov.cn/banshi/wjrs/zscq/wtjd.htm",
-                  "http://www.gov.cn/banshi/zjpq_wtjd.htm"
+                  # "http://www.gov.cn/banshi/wjrs/hysy.htm",
+                  # "http://www.gov.cn/banshi/wjrs/lssf.htm",
+                  # "http://www.gov.cn/banshi/wjrs/lygg.htm",
+                  # "http://www.gov.cn/banshi/wjrs/swtz.htm",
+                  # "http://www.gov.cn/banshi/wjrs/whjy.htm",
+                  # "http://www.gov.cn/banshi/wjrs/ymdj.htm",
+                  # "http://www.gov.cn/banshi/wjrs/zhjy.htm",
+                  # "http://www.gov.cn/banshi/wjrs/zscq.htm",
+                  # "http://www.gov.cn/banshi/zjpq.htm",
+                  # "http://www.gov.cn/banshi/wjrs/crj/wtjd.htm",
+                  # "http://www.gov.cn/banshi/wjrs/hysy/wtjd.htm",
+                  # "http://www.gov.cn/banshi/wjrs/lssf/wtjd.htm",
+                  # "http://www.gov.cn/banshi/wjrs/lygg/wtjd.htm",
+                  # "http://www.gov.cn/banshi/wjrs/swtz/wtjd.htm",
+                  # "http://www.gov.cn/banshi/wjrs/whjy/wtjd.htm",
+                  # "http://www.gov.cn/banshi/wjrs/ymdj/wtjd.htm",
+                  # "http://www.gov.cn/banshi/wjrs/zhjy/wtjd.htm",
+                  # "http://www.gov.cn/banshi/wjrs/zscq/wtjd.htm",
+                  # "http://www.gov.cn/banshi/zjpq_wtjd.htm"
                   ]
 
     links = LinkExtractor(allow="/content")
@@ -49,8 +49,9 @@ class govSpider(CrawlSpider):
         url = response.url
         titles = response.xpath("/html/head/title/text()").extract()
         referUrl = response.request.headers.get('Referer', None)
-        referUrlStr =  str(referUrl, encoding='utf-8')
-        print(titles)
+
+        # referUrlStr =  str(referUrl, encoding='utf-8')  py3
+        referUrlStr = referUrl
 
 
         subStr = referUrlStr.rsplit('/')
@@ -76,7 +77,6 @@ class govSpider(CrawlSpider):
 
         if titles:
 
-            # newPath = self.path + "/haha/aa/aa"
             if not os.path.exists(newPath):
                 os.makedirs(newPath)
 
@@ -91,18 +91,20 @@ class govSpider(CrawlSpider):
 
                 title = sel.xpath("td[@class='txt18']/text()").extract()
                 if title:
-                    file.write(title[0] + "\n")
+
+                    print(title)
+                    file.write(title[0].encode('utf-8') + "\n")
 
             for sel in response.xpath("//tr"):
 
                 source = sel.xpath("td[@class='txt12']/text()").extract()
                 if source:
-                    file.write(source[0] + "\n")
+                    file.write(source[0].encode('utf-8') + "\n")
 
             for sel in response.xpath("//tr"):
 
                 artical = sel.xpath("td[@class='p1']/font[@id='Zoom']/p//text()").extract()
                 if artical:
                     for content in artical:
-                        file.write(content + "\n")
+                        file.write(content.encode('utf-8') + "\n")
 
